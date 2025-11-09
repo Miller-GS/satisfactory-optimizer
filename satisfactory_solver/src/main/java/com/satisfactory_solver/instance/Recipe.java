84fecf6a3779f2b13b5572a57d0a8ee347309385
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2025-11-09 10:04:49 -0300

    feat: improve Recipe class by adding map of resulting item quantities

--- satisfactory_solver/src/main/java/com/satisfactory_solver/instance/Recipe.java
+++ satisfactory_solver/src/main/java/com/satisfactory_solver/instance/Recipe.java
@@ -1,16 +1,27 @@
 package com.satisfactory_solver.instance;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Recipe {
     protected String recipeName;
     protected List<ItemUsage> inputs;
     protected List<ItemUsage> outputs;
+    protected Map<String, Double> resultingItemQuantities;
 
     public Recipe(String recipeName, List<ItemUsage> inputs, List<ItemUsage> outputs) {
         this.recipeName = recipeName;
         this.inputs = inputs;
         this.outputs = outputs;
+        this.resultingItemQuantities = new HashMap<>();
+        for (ItemUsage output : outputs) {
+            resultingItemQuantities.put(output.getItemName(), output.getQuantityPerMinute());
+        }
+        for (ItemUsage input : inputs) {
+            resultingItemQuantities.put(input.getItemName(),
+                    resultingItemQuantities.getOrDefault(input.getItemName(), 0.0) - input.getQuantityPerMinute());
+        }
     }
 
     public String getRecipeName() {
@@ -23,6 +34,10 @@ public class Recipe {
 
     public List<ItemUsage> getOutputs() {
         return outputs;
+    }
+
+    public Double getResultingQuantityForItem(String itemName) {
+        return resultingItemQuantities.getOrDefault(itemName, 0.0);
     }
 
     @Override
